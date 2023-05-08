797ac3c10c80b81cd0938f133413c592d64e27c1
Shengcong.Lin <linshengcong@foxmail.com>
2023-05-08 14:18:35 +0800

    fix: 重构水平垂直间隙功能

--- src/components/flow-chart/event/handleNodeSpacing.js
+++ src/components/flow-chart/event/handleNodeSpacing.js
@@ -2,78 +2,73 @@ export const handleNodeSpacing = (graph, type) => {
   // 获取框选的节点
   const selected = graph.getSelectedCells()
   if (selected.length < 3) return
-  if (type === 'horizontal') {
+  if (type === 'vertical') {
+
     selected.sort((a, b) => a.store.data.position.y - b.store.data.position.y)
+    const topEdge = selected[0].store.data.position.y
+    const lastNode = selected[selected.length - 1]
+    const bottomEdge = lastNode.store.data.position.y + lastNode.store.data.size.height
+    let heightSum = 0 // 所有节点高度总和
     const nodeMap = {}
+    // 数据组装成map, 计算出节点高度总和
     selected.forEach(item => {
 
       const { position, size, id } = item.store.data
       const { x, y } = position
-      const { width, height } = size
-      const bottomPos = y + height
-      const rightPos = x + width
-      nodeMap[id] = { x, y, width, height, id, bottomPos, rightPos }
+      const { height } = size
+      heightSum += height
+      nodeMap[id] = { x, y, height, id }
     })
-    // 最大空隙
-    let maxSpacing = -Infinity
-    // 缓存当前节点底部坐标
-    let cacheBottomPos = -Infinity
-    selected.forEach((item, index) => {
-      const nodeInfo = nodeMap[item.store.data.id]
-      if (!nodeInfo) return console.log('没有这个节点')
-      if (index === 0) return cacheBottomPos = nodeInfo.bottomPos
-      const spacing = nodeInfo.y - cacheBottomPos
-      maxSpacing < spacing && (maxSpacing = spacing)
-      cacheBottomPos = nodeInfo.bottomPos
-    })
-
-    let accumulator = 0
-    // 批量设置节点坐标
+    // 计算出间隙
+    const space = (bottomEdge - topEdge - heightSum) / (selected.length - 1)
+    let yAxis = 0
+    // 缓存上一个节点高度
+    let cachePrevNodeHeight = 0
+    // 重新设置节点位置
     selected.forEach((node, index) => {
+
       const nodeInfo = nodeMap[node.store.data.id]
       if (index === 0) {
-        accumulator = nodeInfo.y
+        yAxis = nodeInfo.y
       } else {
-        accumulator += maxSpacing + nodeInfo['height']
+        yAxis += space + cachePrevNodeHeight
       }
-      node.position(nodeInfo.x, accumulator)
+      cachePrevNodeHeight = nodeInfo['height']
+      node.position(nodeInfo.x, yAxis)
     })
   }
-  else if (type === 'vertical') {
+  else if (type === 'horizontal') {
+    
     selected.sort((a, b) => a.store.data.position.x - b.store.data.position.x)
+    const leftEdge = selected[0].store.data.position.x
+    const lastNode = selected[selected.length - 1]
+    const rightEdge = lastNode.store.data.position.x + lastNode.store.data.size.width
+    let widthSum = 0
     const nodeMap = {}
+
     selected.forEach(item => {
 
       const { position, size, id } = item.store.data
       const { x, y } = position
-      const { width, height } = size
-      const bottomPos = y + height
-      const rightPos = x + width
-      nodeMap[id] = { x, y, width, height, id, bottomPos, rightPos }
-    })
-    // 最大空隙
-    let maxSpacing = -Infinity
-    // 缓存当前节点底部坐标
-    let cacheBottomPos = -Infinity
-    selected.forEach((item, index) => {
-      const nodeInfo = nodeMap[item.store.data.id]
-      if (!nodeInfo) return console.log('没有这个节点')
-      if (index === 0) return cacheBottomPos = nodeInfo.rightPos
-      const spacing = nodeInfo.x - cacheBottomPos
-      maxSpacing < spacing && (maxSpacing = spacing)
-      cacheBottomPos = nodeInfo.rightPos
+      const { width } = size
+      widthSum += width
+      nodeMap[id] = { x, y, width, id }
     })
 
-    let accumulator = 0
-    // 批量设置节点坐标
+    const space = (rightEdge - leftEdge - widthSum) / (selected.length - 1)
+    let xAxis = 0
+    let cachePrevNodeWidth = 0
+
     selected.forEach((node, index) => {
+
       const nodeInfo = nodeMap[node.store.data.id]
       if (index === 0) {
-        accumulator = nodeInfo.x
+        xAxis = nodeInfo.x
       } else {
-        accumulator += maxSpacing + nodeInfo['width']
+        xAxis += space + cachePrevNodeWidth
       }
-      node.position(accumulator, nodeInfo.y)
+      cachePrevNodeWidth = nodeInfo['width']
+      node.position(xAxis, nodeInfo.y)
     })
   }
 }
